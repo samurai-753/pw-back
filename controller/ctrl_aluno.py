9c3bb0faaba29c2ec92934cf1b73f61f189123c3
thuzax <arthurhscruz@gmail.com>
2019-05-22 15:03:11 -0300

    Fixes #1

--- controller/ctrl_aluno.py
+++ controller/ctrl_aluno.py
@@ -3,53 +3,64 @@ from pprint import pprint
 
 class CtrlAluno:
 
+    instancia = None
+
+    class __CtrlAluno:
+
+        def __init__(self):
+            self.alunos = [
+                # Aluno(1, 'Dudei', 'dudu@samurai.io', '(99) 123-123', 'dcc-01', ['bacharel','phd']),
+                # Aluno(43, 'Thuzax', 'Thuzax@samurai.io', '(99) 433-334', 'dcc-43', ['bacharel','phd']),
+            ]
+
+        def add_aluno_p(self, p):
+            l = [ x for x in self.alunos if x.idx == p.idx ]
+            if len(l) > 0:
+                return False
+            
+            self.alunos.append(p)
+
+            return p.idx
+
+        def get_alunos(self):
+            return self.alunos
+
+        def get_aluno(self, idx):
+            p = [ x for x in self.alunos if x.idx ==  idx ]
+            if len(p) < 1:
+                return None
+
+            return p[0]
+        
+        def add_aluno(self, data):
+            p = Aluno.from_dict(data)
+
+            l = [ x for x in self.alunos if x.idx == p.idx ]
+            if len(l) > 0:
+                return False
+            
+            self.alunos.append(p)
+
+            return p.idx
+
+        def update_aluno(self, idx, data):
+            l = [ x for x in self.alunos if x.idx == idx ]
+            if len(l) < 1:
+                return False
+
+            p = l[0]
+            p.set_from_dict(data)
+
+            return True
+        
+        def delete_aluno(self, idx):
+            self.alunos = [ x for x in self.alunos if x.idx != idx ]
+
+            return True
+    
     def __init__(self):
-        self.alunos = [
-            # Aluno(1, 'Dudei', 'dudu@samurai.io', '(99) 123-123', 'dcc-01', ['bacharel','phd']),
-            # Aluno(43, 'Thuzax', 'Thuzax@samurai.io', '(99) 433-334', 'dcc-43', ['bacharel','phd']),
-        ]
+        if(self.instancia == None):
+            self.instancia = self.__CtrlAluno()
 
-    def add_aluno_p(self, p):
-        l = [ x for x in self.alunos if x.idx == p.idx ]
-        if len(l) > 0:
-            return False
-        
-        self.alunos.append(p)
-
-        return p.idx
-
-    def get_alunos(self):
-        return self.alunos
-
-    def get_aluno(self, idx):
-        p = [ x for x in self.alunos if x.idx ==  idx ]
-        if len(p) < 1:
-            return None
-
-        return p[0]
-    
-    def add_aluno(self, data):
-        p = Aluno.from_dict(data)
-
-        l = [ x for x in self.alunos if x.idx == p.idx ]
-        if len(l) > 0:
-            return False
-        
-        self.alunos.append(p)
-
-        return p.idx
-
-    def update_aluno(self, idx, data):
-        l = [ x for x in self.alunos if x.idx == idx ]
-        if len(l) < 1:
-            return False
-
-        p = l[0]
-        p.set_from_dict(data)
-
-        return True
-    
-    def delete_aluno(self, idx):
-        self.alunos = [ x for x in self.alunos if x.idx != idx ]
-
-        return True
+    def __getattr__(self, name):
+        return getattr(self.instancia, name)
