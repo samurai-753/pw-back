0a2b8bb6119fab10cf2ae39910f1f53982bd5798
ribolive <gabriel.ribolive@gmail.com>
2019-06-18 13:16:25 -0300

    Cria testes da rota professor
    * O SQLite roda em memória por padrão

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_marshmallow import Marshmallow
 app = Flask(__name__)
 
 # Config the data base
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI', 'sqlite:///db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DB_URI', 'sqlite://')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
--- tests/test_ctrl_profressor.py
+++ tests/test_ctrl_profressor.py
@@ -33,11 +33,34 @@ class TestCtrlProfessor(unittest.TestCase):
     def dump_professores(self, people):
         return self.schema_professores.dump(people).data
     
+    def get_professor(self, idx):
+        res = self.app.get(
+            '/api/professor/{}'.format(idx)
+        )
+
+        return json.loads(res.data.decode('utf-8'))
+
     def post_professor(self, professor):
         res = self.app.post(
             '/api/professor',
             data=json.dumps(professor),
             content_type='application/json'
+        )
+
+        return json.loads(res.data.decode('utf-8'))
+    
+    def patch_professor(self, idx, dados):
+        res = self.app.patch(
+            '/api/professor/{}'.format(idx),
+            data=json.dumps(dados),
+            content_type='application/json'
+        )
+
+        return json.loads(res.data.decode('utf-8'))
+
+    def delete_professor(self, idx):
+        res = self.app.delete(
+            '/api/professor/{}'.format(idx)
         )
 
         return json.loads(res.data.decode('utf-8'))
@@ -54,3 +77,83 @@ class TestCtrlProfessor(unittest.TestCase):
 
         self.assertEqual(200, data['status'])
         self.assert_professor_equal(data['data'], p)
+    
+    def test__post_professor__400(self):
+        p = self.professores[1]
+        p.pop('sala', None)
+        data = self.post_professor(p)
+
+        self.assertEqual(400, data['status'])
+        self.assertEqual("KeyNotFound 'sala'", data['message'])
+    
+    def test__get_professores__vazio(self):
+        res = self.app.get('/api/professor')
+        data = json.loads(res.data.decode('utf-8'))
+        
+        self.assertEqual(200, data['status'])
+        self.assertFalse(data['data'])
+
+    def test__get_professores__lista(self):
+        for p in self.professores:
+            self.post_professor(p)
+
+        res = self.app.get('/api/professor')
+        data = json.loads(res.data.decode('utf-8'))
+        
+        self.assertEqual(200, data['status'])
+        self.assertEqual(len(self.professores), len(data['data']))
+    
+    def test__get_professor_idx__404(self):
+        data = self.get_professor(42)
+
+        self.assertEqual(404, data['status'])
+        self.assertEqual('IdNotFound 42', data['message'])
+    
+    def test__get_professor_idx__200(self):
+        p = self.professores[0]
+        d = self.post_professor(p)
+        idx = d['data']['idx']
+
+        data = self.get_professor(idx)
+
+        self.assertEqual(200, data['status'])
+        self.assert_professor_equal(data['data'], p)
+    
+    def test__update_professor_idx__404(self):
+        data = self.patch_professor(42, {})    
+
+        self.assertEqual(404, data['status'])
+        self.assertEqual('IdNotFound 42', data['message'])
+    
+    def test__update_professor_idx__200(self):
+        p = self.professores[0]
+        d = self.post_professor(p)
+        idx = d['data']['idx']
+
+        mod = dict(nome='Jão Bara')
+        p['nome'] = 'Jão Bara'
+        data = self.patch_professor(idx, mod)
+
+        self.assertEqual(200, data['status'])
+        self.assert_professor_equal(data['data'], p)
+    
+    def test__delete_professor_idx__404(self):
+        data = self.delete_professor(42)    
+
+        self.assertEqual(404, data['status'])
+        self.assertEqual('IdNotFound 42', data['message'])
+    
+    def test__delete_professor_idx__200(self):
+        p = self.professores[0]
+        data = self.post_professor(p)
+        self.assertEqual(200, data['status'])
+
+        idx = data['data']['idx']
+        
+        data = self.delete_professor(idx)
+        self.assertEqual(200, data['status'])
+
+        data = self.get_professor(idx)
+
+        self.assertEqual(404, data['status'])
+        self.assertEqual('IdNotFound {}'.format(idx), data['message'])
