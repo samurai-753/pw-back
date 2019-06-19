4bb2e0904e346df78b641db53e9eea2bb46c4006
thuzax <arthurhscruz@gmail.com>
2019-06-19 09:39:14 -0300

    Finaliza as excessoes nao encontradas

--- exception/exception_nao_encontrado.py
+++ exception/exception_nao_encontrado.py
@@ -1,27 +1,64 @@
 
+class ExceptionNaoEncontrado(Exception):
+    def __init__(self, nome_classe, campo, valor):
+        self.nome_classe = nome_classe
+        self.campo = campo
+        self.valor = valor
 
-class ExceptionPessoaNaoEncontrado(Exception):
-    def __init__(self, idx=0):
-        Exception.__init__(self)
-        self.idx = idx
-        
     def __str__(self):
-        text = "Erro: pessoa com id=" + str(self.idx) + " não encontrado\n"
-        return text
-
-class ExceptionProfessorNaoEncontrado(ExceptionPessoaNaoEncontrado):
-    def __str__(self):
-        text = "Erro: professor com id=" + str(self.idx) + " não encontrado\n"
+        text = "Erro:" 
+        text += str(self.nome_classe) + " com " + str(self.campo) + "=" + str(self.valor) 
+        text += " não encontrado\n"
         return text
 
 
-class ExceptionAlunoNaoEncontrado(ExceptionProfessorNaoEncontrado):
-    def __str__(self):
-        text = "Erro: aluno com id=" + str(self.idx) + " não encontrado\n"
-        return text
+class ExceptionAlunoNaoEncontrado(ExceptionNaoEncontrado):
+    def __init__(self, campo, valor):
+        ExceptionNaoEncontrado.__init__(self, "Aluno", campo, valor)
+
+
+class ExceptionBibTeXNaoEncontrado(ExceptionNaoEncontrado):
+    def __init__(self, campo, valor):
+        ExceptionNaoEncontrado.__init__(self, "BibTeX", campo, valor)
+
+
+class ExceptionDisciplinaNaoEncontrado(ExceptionNaoEncontrado):
+    def __init__(self, campo, valor):
+        ExceptionNaoEncontrado.__init__(self, "Disciplina", campo, valor)
+
+
+class ExceptionDocumentoNaoEncontrado(ExceptionNaoEncontrado):
+    def __init__(self, campo, valor):
+        ExceptionNaoEncontrado.__init__(self, "Documento", campo, valor)
+
+
+class ExceptionExtensaoNaoEncontrado(ExceptionNaoEncontrado):
+    def __init__(self, campo, valor):
+        ExceptionNaoEncontrado.__init__(self, "Disciplina", campo, valor)
+
+
+class ExceptionPessoaNaoEncontrado(ExceptionNaoEncontrado):
+    def __init__(self, campo, valor):
+        ExceptionNaoEncontrado.__init__(self, "Pessoa", campo, valor)
+
+
+class ExceptionProfessorNaoEncontrado(ExceptionNaoEncontrado):
+    def __init__(self, campo, valor):
+        ExceptionNaoEncontrado.__init__(self, "Professor", campo, valor)
+
+
+class ExceptionProjetoPesquisaNaoEncontrado(ExceptionNaoEncontrado):
+    def __init__(self, campo, valor):
+        ExceptionNaoEncontrado.__init__(self, "ProjetoPesquisa", campo, valor)
+
+
+class ExceptionPublicacaoNaoEncontrado(ExceptionNaoEncontrado):
+    def __init__(self, campo, valor):
+        ExceptionNaoEncontrado.__init__(self, "Publicacao", campo, valor)
+
 
 try:
-    raise ExceptionAlunoNaoEncontrado(10)
+    raise ExceptionPessoaNaoEncontrado('id', 10)
 except Exception as e:
     print(e)
 
